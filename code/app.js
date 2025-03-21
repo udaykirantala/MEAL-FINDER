2989debb1584e53a712a0885e6f9c1b20fcd6f94
udaykirantala <udaykirantala@gmail.com>
2025-03-21 22:55:05 +0530

    Peoject Day1

---
+++ code/app.js
@@ -0,0 +1,8 @@
+function showSlider(){
+    const silder = document.querySelector('.header_heading_slider');
+    silder.style.display='flex';
+}
+function hiddenSlider(){
+    const silder = document.querySelector('.header_heading_slider');
+    silder.style.display='none'
+}
