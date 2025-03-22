de6cbe15e3a32b672266e8a4aaf2313548f2a79f
udaykirantala <udaykirantala@gmail.com>
2025-03-22 14:39:32 +0530

    Main Page Completed

--- code/app.js
+++ code/app.js
@@ -1,8 +1,35 @@
+
+// Slider Functionality Starting
+
 function showSlider(){
     const silder = document.querySelector('.header_heading_slider');
     silder.style.display='flex';
 }
 function hiddenSlider(){
     const silder = document.querySelector('.header_heading_slider');
-    silder.style.display='none'
+    silder.style.display='none';
 }
+
+// Slider Functionality Ending
+
+// Categories Container Content
+
+const containerContent = document.getElementById('categoriesContainerContent');
+const fetchdata = async ()=>{
+    try{
+        const datafetch = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php");
+        const data = await datafetch.json();
+        const categoriesContainerContent = document.getElementById('categoriesContainerContent');
+        categoriesContainerContent.innerHTML=data.categories.map((item)=>{
+            return`
+            <div class="categoriesCards">
+            <img src="${item.strCategoryThumb}" alt="${item.strCategory}">
+            <p>${item.strCategory.toUpperCase()}</p>
+            </div>
+            `
+        }).join("")
+    }catch(error){
+        console.error("Error fetching data:",error)
+    }
+}
+fetchdata()
